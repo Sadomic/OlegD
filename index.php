98ae89c1d4a2469060ee454c5effd6a06f4af60c
Oleg <nutsway1228@mail.ru>
2020-05-31 05:50:29 +0300

    4.0

--- index.php
+++ index.php
@@ -7,3 +7,21 @@ include_once 'Oleg/Linear.php';
 include_once 'Oleg/Square.php';
 include_once 'Oleg/DobriogloException.php';
 include_once 'Oleg/Log.php';
+
+$arr=array();
+
+$arr[] = readline("a= ");
+$arr[] = readline("b= ");
+$arr[] = readline("c= ");
+
+try {
+    $solver = new Alex\Square();
+	$roots = $solver->solve($arr[0], $arr[1], $arr[2]);
+
+    Alex\Log::log("roots: " . implode(" , ", $roots));
+   
+}catch(Alex\BuryaException $e) {
+
+    Alex\Log::log($e->getMessage());
+}
+Alex\Log::write();
