f3ddc80043fdf21a7933dc16affacd241bea6af2
Oleg <nutsway1228@mail.ru>
2020-05-31 05:48:34 +0300

    2.0

--- index.php
+++ index.php
@@ -1,26 +1,55 @@
 <?php
-class A {
-
+// Линейное уравнение a*x + b = 0
+class Linear {
+	
+	protected $x;
+	public function LinearEquation($a, $b) {
+		if($a != 0){
+			return $this->x = -$b /$a;
+			
+		} 
+		return false;
+	}
 }
 
-Class B extends A {
-  public function __construct($obj1) {
-   $this->obj1 = $obj1;
- }
-  protected $obj1;
-}
+// Квадратное уравнение ax^2+bx+c
+class Square extends Linear {
+		//Добавить решение линейного уравнения, если а = 0
+		// Нахождение дискириминанта
+		protected function findDisc($a, $b, $c){
+		    return ($b * $b) - 4 * $a * $c;
+		}
+		
+		// Нахождение корней уравнения
+		public function discEquation($a, $b, $c) {
+		    if($a != 0){
+				$disc = $this->findDisc($a, $b, $c);
 
-Class C extends B {
-  public function __construct($obj1, $obj2) {
-   parent::__construct($obj1);
-    $this->obj2 = $obj2;
-  }
-  protected $obj2;
+				if($disc > 0) {
+					$x1 = (-$b + sqrt($disc))/(2 * $a);
+					$x2 = (-$b - sqrt($disc))/(2 * $a);
+					$this->x = Array($x1, $x2);
+				} elseif($disc == 0) {
+					$this->x = -$b/(2 * $a);
+				} else {
+					$this->x = false;
+				}
+			
+				return $this->x;
+			} else{
+				$this->x = $this->LinearEquation($a, $b);
+			}   
+			return $this->x;
+		}
 }
+/*
+class B extends A {
 
-$obj5 = new A();
-$obj4 = new B($obj5);
-$obj3 = new B($obj5);
-$obj2 = new С($obj5, $obj4);
-$obj1 = new C($obj2, $obj3);
+}
+*/
+$lineareq = new Linear();
+var_dump ($lineareq->LinearEquation(0, 0));
+
+$sqrteq = new Square();
+var_dump($sqrteq->discEquation(0,0,0));
 ?>
